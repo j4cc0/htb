b96f687cbdd2c99b9c6ea9246a8d564eb5486b59
Jacco van Buuren <jacco@chrali.local.lan>
2026-04-24 18:25:59 +0200

    Updated

---
+++ htb-connect-and-scan.sh
@@ -0,0 +1,179 @@
+#!/usr/bin/env bash
+# Script: htb
+# Author: Jacco van Buuren
+# License: BSD 3-clause.
+
+# Description: HackTheBox connect script.
+#   This scripts connect to HTB with openvpn and will setup a directory named after the box.
+#   Nmap is run as soon as the connection is established.
+
+# Parameters:
+# 1. To connect to HTB: OpenVPN-file
+# 2. Box name: String.htb
+# 3. IP-address of the box: 10.129.x.y
+
+# This script will try to:
+# 1. connect to HTB, if not already connected, reconnect if required.
+# 2. When the connection is up, ping box IP-addr or reconnect until it does.
+# 3. If the box has an entry in /etc/hosts: Replace IP-addr with current, if not add IP-addr and name to /etc/hosts
+# 4. mkdir /htb/<name-of-box> if not already there
+# 5. run nmap -A -v -n -vv -p- -Pn --open /htb/<name>/full.nmap IP-addr
+# 6. run whatever
+
+# -- Globals, constants
+
+MYHTBDIR="/htb"
+
+OVPN="${1:-$HOME/openvpn/hackthebox.ovpn}"
+BOXNAME="${2}"
+IP="${3}"
+
+ISCONNECTED=0
+ROUTE=""
+RTR=""
+NIC=""
+
+HTBRTR="10.10.14.1"
+HTBNIC="tun0"
+VPNSLEEP=1
+KILLSLEEP=5
+PINGSLEEP=1
+PINGAMOUNT=10
+OPID=0
+
+HOSTS="/etc/hosts"
+
+# -- Functions
+
+usage() {
+	cat <<-___EOF___
+	Sorry. Not yet implemented.
+	___EOF___
+	return 0
+}
+
+connect() {
+	echo "[+] Connecting to HTB using $OVPN ..."
+	ROUTE=""
+	NIC=""
+	RTR=""
+	RESTARTAFTER="$PINGAMOUNT"
+	OPID=0
+	while [ "$NIC" != "$HTBNIC" -a "$RTR" != "$HTBRTR" ]
+	do
+		if [ $OPID -eq 0 ]; then
+			openvpn "$OVPN" &>/dev/null &
+			OPID="$!"
+		fi
+		sleep "$VPNSLEEP"
+		ROUTE=$(ip route get "$IP" | awk '{print $3 " " $5}' | grep '^[0-9]' | head -n 1)
+		RTR=$(echo "$ROUTE" | awk '{print $1}')
+		NIC=$(echo "$ROUTE" | awk '{print $2}')
+		RESTARTAFTER=$((RESTARTAFTER - 1))
+		if [ "$RESTARTAFTER" -le 0 ]; then
+			echo ""
+			echo "[W] Failed to get a route to $HTBRTR" >&2
+			disconnect
+			RESTARTAFTER="$PINGAMOUNT"
+		fi
+		printf "."
+	done
+	echo ""
+	echo "[+] Route to $HTBRTR established."
+	return 0
+}
+
+disconnect() {
+	echo "[+] Shutting down openvpn ..."
+	pkill -KILL openvpn &>/dev/null
+	OPID=0
+	sleep "$KILLSLEEP"
+}
+
+reconnect() {
+	disconnect
+	connect
+	return 0
+}
+
+# -- Parameter parsing
+
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+	usage
+	exit 0
+fi
+
+# -- Sanity checking
+
+if [ $# -ne 3 ]; then
+	echo "[x] Missing a parameter. Use -h or --help for help. Aborted" >&2
+	exit 1
+fi
+
+if [ ! -r "$OVPN" ]; then
+	echo "[x] $OVPN is not readable. Does it exist? Aborted" >&2
+	exit 1
+fi
+
+# -- Main
+
+# Connect to HTB
+
+OURVPN=$(basename "$OVPN")
+RUNNINGVPN=$(basename "$(ps ax | grep 'openvp[n]' | awk '$5 == "openvpn" { print $NF }' | head -n 1)" 2>/dev/null)
+echo "--- $OURVPN === $RUNNINGVPN"
+if [ "x${RUNNINGVPN}x" = "xx" ]; then
+	# Not connected. Connect first
+	connect
+elif [ "x${RUNNINGVPN}x" != "x${OURVPN}x" ]; then
+	# Connected but to the wrong vpn. Reconnect
+	reconnect
+fi
+
+while [ $ISCONNECTED -eq 0 ]; do
+	PINGCOUNT="$PINGAMOUNT"
+	while [ $ISCONNECTED -eq 0 -a $PINGCOUNT -gt 1 ]; do
+		ping -q -c 1 -w 3 -W 3 "$IP" &>/dev/null && \
+			ISCONNECTED=1
+		printf "."
+		PINGCOUNT=$((PINGCOUNT - 1))
+		sleep "$PINGSLEEP"
+	done
+	# If no ping was answered, restart openvpn.
+	if [ $ISCONNECTED -eq 0 ]; then
+		echo ""
+		echo "[W] Box did not respond. Restarting openvpn." >&2
+		reconnect
+		ISCONNECTED=0
+	fi
+done
+echo ""
+echo "[+] Connected!"
+
+# Add or modify host entry in /etc/hosts
+
+grep -w "$BOXNAME" "$HOSTS" &>/dev/null
+if [ $? -eq 0 ]; then
+	# The box is already in /etc/hosts: Modify the entry to match the current IP-address
+	echo "[+] Modifying $HOSTS to match $BOXNAME to $IP"
+	#sed -i "/$BOXNAME/s/^[^[:space:]]*\(.*$BOXNAME.*$\)/$IP\t\1/" "$HOSTS"
+	sed -i "/$BOXNAME/s/^[^[:space:]]*\($BOXNAME.*\)$/$IP\t\1/" "$HOSTS"
+else
+	# The box is not in /etc/hosts: Add the IP-address and boxname to /etc/hosts
+	echo "[+] Adding $IP and $BOXNAME to $HOSTS"
+	printf "%s\t%s" "$IP" "$BOXNAME" >> /etc/hosts
+fi
+
+# Create directory and start nmap
+
+HTBDIR="$MYHTBDIR/$BOXNAME"
+mkdir -p "$HTBDIR" &>/dev/null
+
+touch "$HTBDIR/$IP" || \
+	{ echo "[x] Failed to write in $HTBDIR. Aborted" >&2; exit 1; }
+
+nmap -A -p- -n -vv -oA "$HTBDIR/full" "$IP"
+
+
+
+
